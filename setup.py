b0e17659b913adbb0690b0dfbed7fffe13baa907
darkclouder <2065342+darkclouder@users.noreply.github.com>
2019-10-14 14:42:05 +0200

    Revert "Fix package name"
    This reverts commit 3556f2f7e36f5da599fcfa5271e13ffdba29918a.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="cli_context",
+    name="cli-context",
     version="0.1.0",
     description="Hierarchical CLI creator",
     long_description="".join([
@@ -17,6 +17,5 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries",
     ],
-    install_requires=[],
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
 )
